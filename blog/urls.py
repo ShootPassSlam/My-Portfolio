a242b828c547150a5d3d9b31c5b0f1793e073594
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-08-19 08:39:15 -0700

    added blog views and urls:

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+     # ex: /blog/
     path('', views.index, name='index'),
+    # ex: /blog/5/
+    path('<int:post_id>/', views.detail, name='detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from .models import Post
+
 def index(request):
 	return HttpResponse("This is where the blog goes")
+
+def detail(request, post_id):
+    return HttpResponse("You're looking at post %s." % post_id)
