8a242b8520f58057a9c28aaaf25855d780bfa7a5
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-08-20 20:19:32 -0700

    test

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -129,3 +130,5 @@ USE_TZ = None
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
