4f9d244f2d80d6318427caad401527d5e003573a
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-08-21 17:52:48 -0700

    tests

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,8 @@ class Post(models.Model):
     post_title = models.CharField(max_length=200)
     post_body = models.TextField(max_length=1000)
     pub_date = models.DateTimeField('date published')
+    def __str__(self):
+        return self.post_body
+    def was_published_in_the_past(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now   
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,18 @@
+import datetime
 from django.test import TestCase
+from django.utils import timezone
 
-# Create your tests here.
+
+from .models import Post
+
+
+class PostModelTests(TestCase):
+
+    def test_was_published_in_the_past(self):
+        """
+        was_published_in_the_past() returns False for posts whose pub_date
+        is in the future.
+        """
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_post = Post(pub_date=time)
+        self.assertIs(future_post.was_published_in_the_past(), False)
