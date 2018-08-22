4a5ee65b6040df0ecbd0a82d9ebeec83108b4d5d
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-08-22 11:18:48 -0700

    add nav bar template and home page

--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="myportfolio/home.html")),
     path('polls/', include('polls.urls')),
     path('blog/', include('blog.urls')),
     path('admin/', admin.site.urls),
