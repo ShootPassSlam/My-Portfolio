38f56b7c2eababea4b2c2f066e51b7fb48b1b012
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-07 09:32:22 -0700

    remove second photo field

--- blog/models.py
+++ blog/models.py
@@ -7,10 +7,9 @@ from django.utils import timezone
 class Post(models.Model):
     post_title = models.CharField(max_length=200)
     post_subtitle = models.CharField(max_length=200, null=True)
-    post_body = HTMLField('Content')
+    post_body = HTMLField('Content', null=True)
     pub_date = models.DateTimeField('date published', null=True)
     primary_photo = models.ImageField(null=True, upload_to='blog/')
-    body_photo = models.ImageField(blank=True, null=True, upload_to='blog/')
     def __str__(self):
         return self.post_body
     def was_published_in_the_past(self):
