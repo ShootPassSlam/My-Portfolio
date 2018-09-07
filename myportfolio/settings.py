c10c4e9e0ffef353c678185d397d5dda6b946243
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-06 18:14:37 -0700

    adjust footer

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -135,8 +135,8 @@ STATICFILES_DIRS = (
 AWS_S3_SECURE_URLS = False
 AWS_STORAGE_BUCKET_NAME = 'dominic.scotto.portfolio.site'
 AWS_S3_REGION_NAME = 'us-west-1'  # e.g. us-east-2
-AWS_ACCESS_KEY_ID = "AWS_ACCESS_KEY_ID"
-AWS_SECRET_ACCESS_KEY = "AWS_SECRET_ACCESS_KEY"
+AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
+AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 # Tell django-storages the domain to use to refer to static files.
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
