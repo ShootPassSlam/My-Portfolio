8d69c3884fb343612d98c2e426886e7ccd29e36a
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-09-07 18:05:52 -0700

    setting up projects model

--- projects/models.py
+++ projects/models.py
@@ -1,15 +1,29 @@
 import datetime
 
 from django.db import models
+from tinymce import HTMLField
+from taggit.managers import TaggableManager
+from taggit.models import TaggedItemBase
 from django.utils import timezone
-from django.contrib.postgres.fields import JSONField
-
 class Project(models.Model):
-
 	project_title = models.CharField(max_length=200)
-	project_description = models.TextField(max_length=1000)
-	tech_used = JSONField()
-	sub_description = JSONField()
+	project_description = HTMLField('Content', null=True)
+	tech_used = TaggableManager()
+	PRODUCTMANAGER = 'PM'
+	WEBDEVELOPER = 'WD'
+	MACHINELEARNING = 'ML'
+	PROJECT_TYPE_CHOICES = (
+		(PRODUCTMANAGER, 'Product Manager'),
+		(WEBDEVELOPER, 'Web Developer'),
+		(MACHINELEARNING, 'Machine Learning'),
+	)
+	project_type = models.CharField(
+		max_length=2,
+		choices=PROJECT_TYPE_CHOICES,
+		default=WEBDEVELOPER,
+	)
 	project_link = models.URLField(max_length=100)
 	project_date = models.DateTimeField('project date')
-	project_photo = models.ImageField()
+	project_photo = models.ImageField(null=True, upload_to='projects/')
+	def __str__(self):
+		return self.project_description
