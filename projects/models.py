734402e38aa9aca9b1146c7e0696fb0fe813367c
Dominic Scotto <dominicscotto@Dominics-MacBook-Pro.local>
2018-10-16 12:01:44 -0700

    fix indentation

--- projects/models.py
+++ projects/models.py
@@ -12,35 +12,35 @@ from django.utils import timezone
 PROJECTS_KEY = "projects.all"
 
 class Project(models.Model):
-	project_title = models.CharField(max_length=200)
-	project_description = HTMLField('Content', null=True)
-	tech_used = TaggableManager()
-	PRODUCTMANAGER = 'PM'
-	WEBDEVELOPER = 'WD'
-	MACHINELEARNING = 'ML'
-	GAMEDEVELOPER = 'GD'
-	PROJECT_TYPE_CHOICES = (
-		(PRODUCTMANAGER, 'Product Manager'),
-		(WEBDEVELOPER, 'Web Developer'),
-		(MACHINELEARNING, 'Machine Learning'),
-		(GAMEDEVELOPER, 'Game Developer'),
-	)
-	project_type = models.CharField(
-		max_length=2,
-		choices=PROJECT_TYPE_CHOICES,
-		default=WEBDEVELOPER,
-	)
-	project_link = models.URLField(max_length=100)
-	project_date = models.DateField('project date')
-	project_photo = models.ImageField(null=True, upload_to='projects/')
-	def __str__(self):
-		return self.project_description
+    project_title = models.CharField(max_length=200)
+    project_description = HTMLField('Content', null=True)
+    tech_used = TaggableManager()
+    PRODUCTMANAGER = 'PM'
+    WEBDEVELOPER = 'WD'
+    MACHINELEARNING = 'ML'
+    GAMEDEVELOPER = 'GD'
+    PROJECT_TYPE_CHOICES = (
+        (PRODUCTMANAGER, 'Product Manager'),
+        (WEBDEVELOPER, 'Web Developer'),
+        (MACHINELEARNING, 'Machine Learning'),
+        (GAMEDEVELOPER, 'Game Developer'),
+    )
+    project_type = models.CharField(
+        max_length=2,
+        choices=PROJECT_TYPE_CHOICES,
+        default=WEBDEVELOPER,
+    )
+    project_link = models.URLField(max_length=100)
+    project_date = models.DateField('project date')
+    project_photo = models.ImageField(null=True, upload_to='projects/')
+    def __str__(self):
+        return self.project_description
 
 @receiver(post_save, sender=Project)
 def invalidate_cache(sender, **kwargs):
-	print("This was called on the update of the project")
-	print("HERE is your cache:")
-	print(cache)
+    print("This was called on the update of the project")
+    print("HERE is your cache:")
+    print(cache)
     cache.delete(PROJECTS_KEY)
     print("your cache should now be deleted:")
     print(cache)
